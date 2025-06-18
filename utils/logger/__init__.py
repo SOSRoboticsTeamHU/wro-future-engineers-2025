81c195be16c6f5646b2fd47b96154efe61386f70
SOS Robotics Team <sos.morse.2024@gmail.com>
2025-06-18 07:42:23 +0200

    logger with log file creation

--- utils/logger/__init__.py
+++ utils/logger/__init__.py
@@ -1,5 +1,18 @@
+import os
 from datetime import datetime
 
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+log_file_path = os.path.join(LOG_DIR, f"robot_{timestamp}.log")
+
 def log(message):
-    timestamp = datetime.now().strftime('%H:%M:%S')
-    print(f'[{timestamp}]: {message}')
+    time_tag = datetime.now().strftime("%H:%M:%S")
+    full_message = (
+        f"[{time_tag}] {message}"
+    )
+    print(full_message)
+
+    with open(log_file_path, "a") as log_file:
+        log_file.write(full_message + "\n")
